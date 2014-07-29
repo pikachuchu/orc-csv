a57282185cc27ecba063eeeb5585d42065f07f12
Maximiline Thayer <garbados@gmail.com>
2014-07-29 16:57:31 -0700

    tests pass

--- test/index.js
+++ test/index.js
@@ -9,11 +9,7 @@ describe('orc-csv', function () {
   before(function () {
     this.filepath = 'test/sample.csv';
     this.collection = 'sample';
-    this.nock = nock("https://api.orchestrate.io")
-    .get('/v0')
-    .reply(200)
-    .post('/v0/' + this.collection)
-    .reply(201);
+    this.nock = nock("https://api.orchestrate.io");
 
     this.orc_csv = orc_csv({
       collection: this.collection,
@@ -21,26 +17,33 @@ describe('orc-csv', function () {
     });
   });
 
-  afterEach(function () {
-    // `restore` only becomes available after intercepting requests
-    if (this.nock.restore) this.nock.restore();
-  });
-
   it('should sync from a path name', function (done) {
     var self = this;
+    
+    this.nock
+    .post('/v0/' + this.collection).reply(201)
+    .post('/v0/' + this.collection).reply(201);
+
     this.orc_csv.upload().file(this.filepath)
     .then(function () {
       self.nock.done();
+      done();
     })
     .fail(done);
   });
 
   it('should sync from a file stream', function (done) {
     var self = this;
+
+    this.nock
+    .post('/v0/' + this.collection).reply(201)
+    .post('/v0/' + this.collection).reply(201);
+
     var stream = fs.createReadStream(path.resolve(this.filepath));
     this.orc_csv.upload().stream(stream)
     .then(function () {
       self.nock.done();
+      done();
     })
     .fail(done);
   });
