12ef8ad7c273fbc9fc3bfe46236a8e0d2365835e
Maximiline Thayer <garbados@gmail.com>
2014-07-29 13:08:44 -0700

    initial tests

---
+++ test/index.js
@@ -0,0 +1,17 @@
+var orc_csv = require('../lib-cov');
+var fs = require('fs');
+var nock = require('nock');
+
+describe('orc-csv', function () {
+  it('should sync from a path name', function () {
+
+  });
+
+  it('should sync from a file stream', function () {
+
+  });
+
+  it('should start a web server containing uploaded docs', function () {
+
+  });
+});
