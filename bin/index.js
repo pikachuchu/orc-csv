368fc1313c655f3c1108cadeadfb60537cff0df5
Justin Mecham <justin@mecham.me>
2015-07-22 15:18:21 -0400

    Added support for multiple datacenters.

--- bin/index.js
+++ bin/index.js
@@ -11,21 +11,24 @@ var argv = require('yargs')
   .alias('h', 'help')
   .alias('v', 'version')
   .alias('u', 'api-key')
+  .alias('d', 'datacenter')
   .alias('f', 'file')
   .alias('c', 'collection')
   .alias('p', 'port')
   // descriptions
-  .describe('u', 'An Orchestrate API key. Defaults to $ORCHESTRATE_API_KEY')
-  .describe('f', 'Path to the CSV to upload')
-  .describe('c', 'Name of the Orchestrate collection to upload files into')
+  .describe('u', 'An Orchestrate API key. Defaults to $ORCHESTRATE_API_KEY.')
+  .describe('d', 'An Orchestrate datacenter host. Defaults to $ORCHESTRATE_API_HOST.')
+  .describe('f', 'Path to the CSV to upload.')
+  .describe('c', 'Name of the Orchestrate collection to upload files into.')
   .describe('p', 'Port for `orc-csv server` to listen on.')
   // examples
-  .example('$0 -u API_KEY -f PATH/TO/FILE -c COLLECTION_NAME')
-  .example('$0 server -u API_KEY')
+  .example('$0 -u API_KEY -e API_HOST -f PATH/TO/FILE -c COLLECTION_NAME')
+  .example('$0 server -u API_KEY -e API_HOST')
   .argv;
 
 var orc = orc_csv({
   api_key: argv.u || process.env.ORCHESTRATE_API_KEY,
+  api_host: argv.d,
   collection: argv.c
 });
 
